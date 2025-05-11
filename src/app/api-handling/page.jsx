e4d344e21745944a6948450265ccc33d9955d93e
Akash <akash.v.harale@gmail.com>
2025-05-11 18:00:48 +0530

    api handling practice

--- src/app/api-handling/page.jsx
+++ src/app/api-handling/page.jsx
@@ -1,3 +1,61 @@
+// "use client";
+
+// import React, { useEffect, useState } from "react";
+// import { useFetchData } from "../hooks/useFetchData";
+
+// const ApiHandling = () => {
+//   const [query, setQuery] = useState("");
+//   const [debouncedQuery, setDebouncedQuery] = useState("");
+
+//   const [ data, error, loading ] = useFetchData(
+//     `http://localhost:4000/products?search=${debouncedQuery}`
+//   );
+
+//   // Debounce query
+//   useEffect(() => {
+//     let timerId;
+//     timerId = setTimeout(() => {
+//       setDebouncedQuery(query);
+//     }, 2000);
+
+//     return () => clearTimeout(timerId);
+//   }, [query]);
+
+//   if (error) {
+//     return <div>somethig went wrong</div>;
+//   }
+
+//   if (loading) {
+//     return "loading...";
+//   }
+
+//   return (
+//     <div>
+//       <h1 className="text-2xl text-center">
+//         Number of products {data?.length}
+//       </h1>
+//       <label htmlFor="">Search products : </label>
+//       <input
+//         type="text"
+//         value={query}
+//         onChange={(e) => setQuery(e.target.value)}
+//         className="border border-orange-500"
+//       />
+
+//       {data?.length >= 1 && (
+//         <div>
+//           <h1 className="text-2xl text-center"> Products Data</h1>
+//           {data.map((el) => {
+//             return <div key={el.id}>{el.name}</div>;
+//           })}
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default ApiHandling;
+
 "use client";
 
 import React, { useEffect, useState } from "react";
@@ -7,46 +65,45 @@ const ApiHandling = () => {
   const [query, setQuery] = useState("");
   const [debouncedQuery, setDebouncedQuery] = useState("");
 
-  const { data, error, loading } = useFetchData(
+  const [data, error, loading] = useFetchData(
     `http://localhost:4000/products?search=${debouncedQuery}`
   );
 
-  // Debounce query
   useEffect(() => {
     let timerId;
-    timerId = setTimeout(() => {
+    setTimeout(() => {
       setDebouncedQuery(query);
     }, 2000);
 
     return () => clearTimeout(timerId);
   }, [query]);
 
-  if (error) {
-    return <div>somethig went wrong</div>;
-  }
-
   if (loading) {
-    return "loading...";
+    return <h1 className="text-3xl text-blue-400">Loading...</h1>;
   }
 
+  if (error) {
+    return <h1 className="text-3xl text-blue-400"> something went wrong...</h1>;
+  }
   return (
     <div>
-      <h1 className="text-2xl text-center">
-        Number of products {data?.length}
-      </h1>
-      <label htmlFor="">Search products : </label>
+      <h1 className="text-2xl text-center">Api Handling</h1>
       <input
+        className="text-center border border-orange-300"
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="border border-orange-500"
       />
 
       {data?.length >= 1 && (
         <div>
-          <h1 className="text-2xl text-center"> Products Data</h1>
+          <h1>This is the products list</h1>
           {data.map((el) => {
-            return <div key={el.id}>{el.name}</div>;
+            return (
+              <div key={el.id}>
+                <h1>{el.name}</h1>
+              </div>
+            );
           })}
         </div>
       )}
--- src/app/hooks/useFetchData.js
+++ src/app/hooks/useFetchData.js
@@ -1,28 +1,55 @@
-"use client";
+// "use client";
 
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+
+// export const useFetchData = (url) => {
+//   const [data, setData] = useState(null);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(false);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         setLoading(true);
+//         const response = await axios.get(url);
+//         setData(response.data);
+//       } catch (error) {
+//         setError(true);
+//         console.log(error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+//     fetchData();
+//   }, [url]);
+
+//   return { data, error, loading };
+// };
+
 export const useFetchData = (url) => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    const fetchData = async () => {
+    (async () => {
       try {
         setLoading(true);
+        setError(false);
         const response = await axios.get(url);
         setData(response.data);
       } catch (error) {
         setError(true);
-        console.log(error);
+        console.log(error, `something went wrong`);
       } finally {
         setLoading(false);
+        setError(false);
       }
-    };
-    fetchData();
+    })();
   }, [url]);
-
-  return { data, error, loading };
+  return [data, error, loading];
 };
